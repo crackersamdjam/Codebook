476de3ced98a14a8afc7c274ccaa332beaa775e1
crackersamdjam <crackersamdjam@gmail.com>
2021-04-01 16:35:06 -0400

    Add aplusb test

---
+++ tests/verify/lc_aplusb.cpp
@@ -0,0 +1,12 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+
+int main(){
+	int a, b;
+	cin>>a>>b;
+	cout<<a+b<<'\n';
+}
