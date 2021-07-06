aaa68ba5b9e0ffcd96ed36358771e5e2b799f9e9
crackersamdjam <45298690+crackersamdjam@users.noreply.github.com>
2021-07-05 21:49:47 -0400

    Add lazy segment tree

--- content/data_structures/lazy_segment_tree.h
+++ content/data_structures/lazy_segment_tree.h
@@ -2,45 +2,67 @@
 #include "../utils/template.h"
 
 /**
- * @brief Segment Tree
- * @docs docs/segment_tree.md
+ * @brief Lazy Segment Tree
+ * @docs docs/lazy_segment_tree.md
  * @info
+ *
+ * todo: make "find first in range" (from cp algorthms) function
+ * to do that, I need a function to check if f(x, current) is good enough, and I also need combine(current, val[rt])
+ *
  */
 
-template<class T, class L> struct segment_tree{
+template<class T, class L> struct lazy_segment_tree{
 	int SZ;
 	vector<T> val;
+	vector<L> lp;
 	function<T(T, T)> merge;
-	function<T(T, L)> apply;
+	function<T(T, L, int, int)> apply;
+	function<L(L, L)> merge_lazy;
 	
-	void init(int n, T def, function<T(T, T)> f, function<T(T, L)> g){
+	void init(int n, T def, L ldef, function<T(T, T)> f, function<T(T, L, int, int)> g, function<L(L, L)> h){
 		SZ = n;
 		val.resize(__builtin_popcount(n) == 1 ? 2*n : 4*n, def);
+		lp.resize(size(val), ldef);
 		// If n is a power of 2, only 2n is needed. Otherwise 4n.
 		merge = f;
 		apply = g;
+		merge_lazy = h;
 	}
 
 	#define lc (rt<<1)
 	#define rc (rt<<1|1)
 	#define nm ((nl+nr)/2)
+
+	void push(int nl, int nr, int rt){
+		L &x = lp[rt];
+		val[lc] = apply(val[lc], x, nl, nm);
+		lp[lc] = merge_lazy(lp[lc], x);
+		val[rc] = apply(val[rc], x, nm+1, nr);
+		lp[rc] = merge_lazy(lp[rc], x);
+		x = lp[0];
+	}
 	
 	T _query(int l, int r, int nl, int nr, int rt){
 		if(r < nl or nr < l) return val[0];
 		if(l <= nl and nr <= r) return val[rt];
+		push(nl, nr, rt);
 		return merge(_query(l, r, nl, nm, lc), _query(l, r, nm+1, nr, rc));
 	}
 	T query(int l, int r){ return _query(l, r, 0, SZ-1, 1); }
 	
-	void _update(int i, L x, int nl, int nr, int rt){
-		if(nl == nr){
-			val[rt] = apply(val[rt], x);
+	void _update(int l, int r, L x, int nl, int nr, int rt){
+		if(r < nl or nr < l) return;
+		if(l <= nl and nr <= r){
+			val[rt] = apply(val[rt], x, nl, nr);
+			lp[rt] = merge_lazy(lp[rt], x);
 			return;
 		}
-		i <= nm ? _update(i, x, nl, nm, lc) : _update(i, x, nm+1, nr, rc);
+		push(nl, nr, rt);
+		_update(l, r, x, nl, nm, lc);
+		_update(l, r, x, nm+1, nr, rc);
 		val[rt] = merge(val[lc], val[rc]);
 	}
-	void update(int i, L x){ _update(i, x, 0, SZ-1, 1); };
+	void update(int l, int r, L x){ _update(l, r, x, 0, SZ-1, 1); };
 
 	#undef lc
 	#undef rc
--- tests/lazy_segment_tree.test.cpp
+++ tests/lazy_segment_tree.test.cpp
@@ -1,8 +1,8 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
-#include "../content/data_structures/segment_tree.h"
+#define PROBLEM "https://judge.yosupo.jp/problem/range_affine_range_sum"
+#include "../content/data_structures/lazy_segment_tree.h"
 
 using ll = long long;
-using p = pair<ll, ll>;
+using pll = pair<ll, ll>;
 constexpr ll mod = 998244353;
 
 int main(){
@@ -13,29 +13,31 @@ int main(){
 	int n, q;
 	cin>>n>>q;
 	
-	segment_tree<p, p> ST;
-	ST.init(n+5, {1, 0},
-		[](auto x, auto y){ return p(x.first*y.first % mod, (y.first*x.second + y.second) % mod); },
-		[](auto x, auto y){ return y; });
+	lazy_segment_tree<ll, pll> ST;
+	ST.init(n+5, 0, pll(1, 0),
+		[](auto x, auto y){ return (x+y) % mod; },
+		[](auto x, auto y, int l, int r){ return (x*y.first + (r-l+1)*y.second) % mod; },
+		[](auto x, auto y){ return pll(x.first*y.first % mod, (y.first*x.second + y.second) % mod); }
+		);
 	
 	for(int i = 0; i < n; i++){
-		ll a, b;
-		cin>>a>>b;
-		ST.update(i, {a, b});
+		ll a;
+		cin>>a;
+		ST.update(i, i, {0, a});
 	}
 	while(q--){
 		int op;
 		cin>>op;
 		if(op == 0){
-			int i; ll a, b;
-			cin>>i>>a>>b;
-			ST.update(i, {a, b});
+			int l, r; ll b, c;
+			cin>>l>>r>>b>>c;
+			ST.update(l, r-1, {b, c});
 		}
 		else{
-			int l, r; ll x;
-			cin>>l>>r>>x;
-			auto [a, b] = ST.query(l, r-1);
-			cout<<(a*x+b)%mod<<'\n';
+			int l, r;
+			cin>>l>>r;
+			auto a = ST.query(l, r-1);
+			cout<<a<<'\n';
 		}
 	}
 }
